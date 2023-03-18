b56d1a4fc4508a34361ea85a11c72131abcb0027
Maja Nowakowska <radom.maja.nowakowska@gmail.com>
2023-03-18 12:58:54 +0100

    dodano działającą grę w trybie dwuosobowym

--- gra/script.js
+++ gra/script.js
@@ -1,83 +1,92 @@
-const gameStatus = document.querySelector('#gamestatus');
-let gameActive = true;
-let currentPlayer = "X";
-let gameState = ["", "", "", "", "", "", "", "", ""];
-const winMessage = () =>'Zwyciężył gracz ${currentPlayer}.';
-const drawMessage = () => 'Remis.'
-const playerTurn = () => 'Tura gracza ${currentPlayer}';
-const winningCondition = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
-]
-gameStatus.innerHTML = playerTurn();
+const cells = document.querySelectorAll("[cell-index]");
+const board = document.getElementById("gamecontainer");
+const gameStatus = document.getElementById("gamestatus");
+const restartButton = document.getElementById("gamerestart");
+let OplayerTurn = false;
+let running = false;
+const playerX = "X";
+const playerO = "O";
+const winningCombination = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
-function cellPlayed(){
-    gameState[clickedCellIndex] = currentPlayer;
-    clickedCell.innerHTML = currentPlayer;
+startGame();
+restartButton.addEventListener("click", startGame);
+
+function startGame() {
+  running = true;
+  OplayerTurn = false;
+  cells.forEach((cell) => {
+    cell.classList.remove(playerX);
+    cell.classList.remove(playerO);
+    cell.removeEventListener("click", handleCellClick);
+    cell.addEventListener("click", handleCellClick, { once: true });
+  });
+  setBoardHoverClass();
+  gameStatus.innerText = `Tura gracza ${OplayerTurn ? "O" : "X"}`;
 }
 
-function playerChange(){
-    currentPlayer = currentPlayer === "X" ? "O" : "X";
-    gameStatus.innerHTML = currentPlayerTurn();
-}
-
-function resultValidation(){
-    let roundWon = false;
-    for (let i=0; i<=7; i++){
-        const win = winningCondition[i];
-        let a = gameState[win[0]];
-        let b = gameState[win[1]];
-        let c = gameState[win[2]];
-        if(a==='' || b==='' || c===''){
-            continue;
-        }
-        if (a === b && b === c){
-            roundWon = true;
-            break
-        }
+function handleCellClick(e) {
+  if (running) {
+    const cell = e.target;
+    const currentPlayer = OplayerTurn ? playerO : playerX;
+    placeMark(cell, currentPlayer);
+    if (checkWin(currentPlayer)) {
+      endGame(false);
+    } else if (isDraw()) {
+      endGame(true);
+    } else {
+      swapTurns();
+      setBoardHoverClass();
     }
-    if (roundWon){
-        gameStatus.innerHTML = winMessage();
-        gameActive = false;
-        return;
-    }
-
-    let roundDraw = !gameState.includes("");
-    if (roundDraw){
-        gameStatus.innerHTML = drawMessage();
-        gameActive = false;
-        return;
-    }
-
-    playerChange();
+  }
 }
 
-function cellClick(clickedCellEvent){
-    const clickedCell = clickedCellEvent.target;
-    const clickedCellIndex = parseInt(
-        clickedCell.getAttribute('cell-index')
-    );
-    if (gameState[clickedCellIndex] !== "" || !gameActive){
-        return;
-    }
-
-    cellPlayed(clickedCell, clickedCellIndex);
-    resultValidation();
+function endGame(draw) {
+  if (draw) {
+    gameStatus.innerText = "Remis";
+  } else {
+    gameStatus.innerText = `Gracz ${OplayerTurn ? "O" : "X"} wygrywa`;
+  }
+  running = false;
 }
 
-function restartGame(){
-    gameActive = true;
-    currentPlayer = "X";
-    gameState = ["", "", "", "", "", "", "", "", ""];
-    gameStatus.innerHTML = currentPlayerTurn();
-    document.querySelectorAll('#cell').forEach(cell => cell.innerHTML = "");
+function isDraw() {
+  return [...cells].every((cell) => {
+    return cell.classList.contains(playerX) || cell.classList.contains(playerO);
+  });
 }
 
-document.querySelectorAll('#cell').forEach(cell => cell.addEventListener('click', cellClick));
-document.querySelector("#gamerestart").addEventListener('click', restartGame);
+function placeMark(cell, currentPlayer) {
+  cell.classList.add(currentPlayer);
+}
+
+function swapTurns() {
+  OplayerTurn = !OplayerTurn;
+  gameStatus.innerText = `Tura gracza ${OplayerTurn ? "O" : "X"}`;
+}
+
+function setBoardHoverClass() {
+  board.classList.remove(playerX);
+  board.classList.remove(playerO);
+  if (OplayerTurn) {
+    board.classList.add(playerO);
+  } else {
+    board.classList.add(playerX);
+  }
+}
+
+function checkWin(currentPlayer) {
+  return winningCombination.some((combination) => {
+    return combination.every((index) => {
+      return cells[index].classList.contains(currentPlayer);
+    });
+  });
+}
