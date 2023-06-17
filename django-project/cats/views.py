0a7e80aea0b04a21e7d437ee7c6bdb7050fc2e78
Maja Nowakowska <radom.maja.nowakowska@gmail.com>
2023-06-17 18:39:20 +0200

    dodano pobieranie contentu strony z bazy (tylko pl)

--- django-project/cats/views.py
+++ django-project/cats/views.py
@@ -1,14 +1,15 @@
 from django.http import HttpResponse
 from django.template import loader
-from .models import Cat
+from .models import Cat, WebsiteContent
 from django.shortcuts import render
 
 
 def cats(request):
   cats = Cat.objects.all().values()
+  content = WebsiteContent.objects.all().filter(language__language='pl')[0]
   template = loader.get_template('index.html')
   context = {
     'cats' : cats,
+    'content': content,
   }
   return HttpResponse(template.render(context, request))
-
